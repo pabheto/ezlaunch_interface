252c6c087826eff028725d055f67ecf34812c45a
degencreator <156078277+degencreator@users.noreply.github.com>
2024-08-15 00:40:42 +0200

    trading terminal layout + app src refactor

---
+++ src/app/(default)/trading/page.tsx
@@ -0,0 +1,30 @@
+import TradingTerminalChart from "@/components/trading/TradingTerminalChart";
+
+export default function Trading() {
+  const initialData = [
+    { time: "2018-12-22", value: 32.51 },
+    { time: "2018-12-23", value: 31.11 },
+    { time: "2018-12-24", value: 27.02 },
+    { time: "2018-12-25", value: 27.32 },
+    { time: "2018-12-26", value: 25.17 },
+    { time: "2018-12-27", value: 28.89 },
+    { time: "2018-12-28", value: 25.46 },
+    { time: "2018-12-29", value: 23.92 },
+    { time: "2018-12-30", value: 22.68 },
+    { time: "2018-12-31", value: 22.67 },
+  ];
+
+  return (
+    <>
+      <div className="trading-header">
+        <div className="trading-symbol">Symbol PACO/USDT</div>
+      </div>
+      <div className="trading-layout grid grid-cols-2 gap-2">
+        <div className="trading-chart-container ">
+          <TradingTerminalChart data={initialData} />
+        </div>
+        <div className="trading-actions-container">Trading controls</div>
+      </div>
+    </>
+  );
+}
